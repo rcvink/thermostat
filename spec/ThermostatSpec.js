d213d1aeeca389cdf293c0b5533fe1e69d6dddad
rcvink <rrvink@gmail.com>
2018-01-31 15:55:29 +0000

    Basic functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,6 +6,24 @@ describe('Thermostat', function() {
   });
 
   it('is at 20 degrees initially', function() {
-    expect(thermostat.temperature()).toEqual(20)
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  describe('up', function() {
+    it('increases temperature', function() {
+      thermostat.up(5);
+      expect(thermostat.temperature()).toEqual(25);
+    });
+  });
+
+  describe('down', function() {
+    it('decreases temperature', function() {
+      thermostat.down(5);
+      expect(thermostat.temperature()).toEqual(15);
+    });
+
+    it('does not decrease temperature below 10 degrees', function() {
+      expect(function() {thermostat.down(11)}).toThrowError('Cannot decrease temperature below 10 degrees');
+    });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,21 @@
 function Thermostat() {
-  this._temperature = 20
+  this._temperature = 20;
+  this._minimumTemperature = 10;
 };
 
+const MIN_TEMPERATURE = 10;
+
 Thermostat.prototype.temperature = function() {
-  return this._temperature
-}
+  return this._temperature;
+};
+
+Thermostat.prototype.up = function(temperatureIncrease) {
+  this._temperature += temperatureIncrease;
+};
+
+Thermostat.prototype.down = function(temperatureDecrease) {
+  if (this._temperature - temperatureDecrease < MIN_TEMPERATURE) {
+    throw Error('Cannot decrease temperature below 10 degrees');
+  };
+  this._temperature -= temperatureDecrease;
+};
